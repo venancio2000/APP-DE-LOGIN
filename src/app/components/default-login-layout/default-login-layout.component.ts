fad93be8628f87b2621335f4b896a6692c92eaa5
venancio2000 <jbrunogvenancio@gmail.com>
2025-05-17 00:09:48 -0300

    implementando layout de cadastro

---
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, input } from '@angular/core';
+
+@Component({
+  selector: 'app-default-login-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './default-login-layout.component.html',
+  styleUrl: './default-login-layout.component.css'
+})
+export class DefaultLoginLayoutComponent {
+@Input() title: string = "";
+@Input() primaryBtnText = "";
+@Input() secondaryBtnText = "";
+}
