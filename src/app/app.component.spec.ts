70d3b336e0abd9dd0854aaf351a5fc37d4434fc1
Bruno Venâncio <bruno-venancio@spgg.rs.gov.br>
2025-06-12 23:41:55 -0300

    implemetacao ajsutes no login, funcionando

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'login-pagin' title`, () => {
+  it(`should have the 'login-page' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('login-pagin');
+    expect(app.title).toEqual('login-page');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, login-pagin');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, login-page');
   });
 });
